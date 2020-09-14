94a1e2639836bc9e93969989327f95605f51c45d
Fares <fares.osman@outlook.com>
2020-09-13 22:48:46 -0400

    Render Root component upon DOMContentLoaded

--- app/javascript/packs/ez_commerce.jsx
+++ app/javascript/packs/ez_commerce.jsx
@@ -8,18 +8,19 @@ import SessionForm from "../components/session_form/session_form";
 import { signUpUser, updateUser } from "../util/session_api_util";
 import { update, signup, login, logout } from "../actions/session_actions";
 import configureStore from "../store/store";
+import Root from "../components/root";
 
 document.addEventListener("DOMContentLoaded", () => {
+  const store = configureStore();
+  window.signup = signup;
+  window.update = update;
+  window.login = login;
+  window.logout = logout;
+  window.dispatch = store.dispatch;
+  window.getState = store.getState;
+
   ReactDOM.render(
-    <SessionForm />,
+    <Root />,
     document.body.appendChild(document.createElement("div"))
   );
 });
-
-const store = configureStore();
-window.signup = signup;
-window.update = update;
-window.login = login;
-window.logout = logout;
-window.dispatch = store.dispatch;
-window.getState = store.getState;
