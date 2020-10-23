6eff60abebc53d7c49711d8e11761a79cb883a78
Fares <fares.osman@outlook.com>
2020-10-22 23:50:43 -0400

    Add the number of reviews to render function of product show

--- app/javascript/components/product/product_show.jsx
+++ app/javascript/components/product/product_show.jsx
@@ -44,7 +44,7 @@ class ProductShow extends React.Component {
           <div>
             <h2>{this.props.product.title}</h2>
             <h2>{this.props.product.price}</h2>
-            <div>X stars (X reviews)</div>
+            <div>X stars ({this.props.productReviews.length} reviews)</div>
             <button type="button">-</button>
             <input type="text" value="1" onChange={this.handleQuantity}></input>
             <button type="button">+</button>
