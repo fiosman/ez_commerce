1e789ce277021346d2d8b760fc95fd94f5c7b585
Fares <fares.osman@outlook.com>
2020-10-25 20:10:12 -0400

    Write validations for review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,7 @@
 class Review < ApplicationRecord
   validates_presence_of :body, :user_id, :product_id, :rating
+  validates_numericality_of :rating, only_integer: true, message: "must be of valid format (i.e. 3 or 5)"
+  validates_inclusion_of :rating, in: 1..5, message: "must be between 1 and 5 (inclusive)"
 
   belongs_to :author,
     foreign_key: :user_id,
