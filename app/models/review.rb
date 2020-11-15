a7275baefef307fd37a174d0cac6d3cde0c975ab
Fares <fares.osman@outlook.com>
2020-11-14 21:03:22 -0500

    Modify review model so only one product review per user is permitted

--- app/models/review.rb
+++ app/models/review.rb
@@ -21,6 +21,7 @@ class Review < ApplicationRecord
   validates_presence_of :body, :user_id, :product_id, :rating
   validates_numericality_of :rating, only_integer: true, message: "must be of valid format (i.e. 3 or 5)"
   validates_inclusion_of :rating, in: 1..5, message: "must be between 1 and 5 (inclusive)"
+  validates_uniqueness_of :user_id, scope: :product_id, message: "has already reviewed this product!"
 
   belongs_to :author,
     foreign_key: :user_id,
