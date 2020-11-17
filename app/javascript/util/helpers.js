ff6d2b74c1efbf99dcec0fc21694447a029ca0b1
Fares <fares.osman@outlook.com>
2020-11-16 21:07:56 -0500

    Fix priceFilter bug

--- app/javascript/util/helpers.js
+++ app/javascript/util/helpers.js
@@ -1,14 +1,14 @@
 export const byPrice = (productPrice, filterPrice = "") => {
-  let parsedPrice = parseInt(productPrice);
+  let parsedPrice = parseFloat(productPrice);
   switch (filterPrice) {
     case "<25":
       return parsedPrice < 25 ? true : false;
     case "25-50":
-      return parsedPrice > 25 && parsedPrice < 50 ? true : false;
+      return parsedPrice >= 25 && parsedPrice <= 50 ? true : false;
     case "50-100":
-      return parsedPrice > 50 && parsedPrice < 100 ? true : false;
+      return parsedPrice >= 50 && parsedPrice < 100 ? true : false;
     case ">100":
-      return parsedPrice > 100 || parsedPrice === 100 ? true : false;
+      return parsedPrice > 100 ? true : false;
     default:
       return true;
   }
