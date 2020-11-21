c71da5e797eed834bd9b1a7476437fee6b2e2bea
Fares <fares.osman@outlook.com>
2020-11-21 17:45:46 -0500

    write add_line_items in cart model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -15,13 +15,9 @@ class Cart < ApplicationRecord
   after_initialize :set_cart_token
   validates_presence_of :cart_token
 
-  def add_item(product_params)
-    current_item = self.line_items.find_by(product_id: product_params[:line_item][:product_id])
-    if current_item
-      current_item.quantity += product_params[:line_item][:quantity]
-      current_item.save
-    else
-      new_item = self.line_items.create(product_id: product_params[:line_item][:product_id])
+  def add_line_items(line_items)
+    line_items.each do |line_item|
+      self.line_items.create(line_item.attributes)
     end
   end
 
