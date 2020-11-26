7b86d41e9be98b8899a253e3d2f015e9092b5222
Fares <fares.osman@outlook.com>
2020-11-25 22:19:52 -0500

    Clean up commens in entry file

--- app/javascript/packs/ez_commerce.jsx
+++ app/javascript/packs/ez_commerce.jsx
@@ -1,20 +1,9 @@
-// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
-// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
-// of the page.
-
 import React from "react";
 import ReactDOM from "react-dom";
 import { store } from "../store/store";
 import Root from "../components/root";
-import { addReview, removeReview } from "../actions/review_actions";
-import { fetchProducts } from "../actions/product_actions";
-import { pullCategories } from "../actions/category_actions";
 
 document.addEventListener("DOMContentLoaded", () => {
-  window.store = store;
-  window.dispatch = store.dispatch;
-  window.addReview = addReview;
-  window.removeReview = removeReview;
   ReactDOM.render(
     <Root store={store} />,
     document.body.appendChild(document.createElement("div"))
