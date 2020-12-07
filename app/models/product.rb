b0031e786995303bbbe4be9e67763b06d057f4d6
Fares <fares.osman@outlook.com>
2020-12-06 19:48:45 -0500

    Add default photo to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -21,7 +21,19 @@ class Product < ApplicationRecord
   has_many :line_items, dependent: :destroy
   has_one_attached :image
 
+  after_commit :ensure_image_attached, on: [:create]
+
   def average_rating
     self.reviews.count > 0 ? self.reviews.average(:rating).to_f.round : 0
+  end
+
+  private
+
+  def ensure_image_attached
+    unless self.image.attached?
+      self.image.attach(io: File.open(Rails.root.join("app", "assets", "images", "default", "default_product.png")),
+                        filename: "default_product.png",
+                        content_type: "image/jpg")
+    end
   end
 end
