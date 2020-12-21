63eca40fccd2c2d7317880eb35efe02f29cbef3e
Fares <fares.osman@outlook.com>
2020-12-21 13:01:32 -0500

    Modify product migrations

--- db/migrate/20201002211700_add_title_to_products.rb
+++ db/migrate/20201002211700_add_title_to_products.rb
@@ -1,5 +1,5 @@
 class AddTitleToProducts < ActiveRecord::Migration[6.0]
-  def change
-    add_column :products, :title, :string, null: false
-  end
+  # def change
+  #   # add_column :products, :title, :string, null: false
+  # end
 end
