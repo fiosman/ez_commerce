754ce58297b658560a3a753593b49c43879027e9
Fares <fares.osman@outlook.com>
2020-12-29 01:17:48 -0500

    Write lineItemsReducer tests for handling default state and RECEIVE_LINE_ITEM

--- app/javascript/__tests__/line_items_reducer.test.js
+++ app/javascript/__tests__/line_items_reducer.test.js
@@ -1,35 +1,56 @@
 import {
-  RECEIVE_ALL_LINE_ITEMS,
   RECEIVE_LINE_ITEM,
   REMOVE_LINE_ITEM,
   UPDATE_LINE_ITEM,
 } from "../actions/line_item_actions";
 import lineItemsReducer from "../reducers/line_items_reducer";
 
-// describe("LineItems Reducer", () => {
-//   test("has a default state", () => {
-//     expect(
-//       usersReducer(undefined, {
-//         type: "non valid action type",
-//       })
-//     ).toEqual({});
-//   });
+describe("LineItems Reducer", () => {
+  const state = {
+    1: {
+      id: 1,
+      cart_id: 3,
+      product_id: 5,
+      quantity: 4,
+    },
+    2: {
+      id: 2,
+      cart_id: 3,
+      product_id: 7,
+      quantity: 5,
+    },
+  };
 
-//   test("can handle RECEIVE_CURRENT_USER", () => {
-//     expect(
-//       usersReducer(undefined, {
-//         type: RECEIVE_CURRENT_USER,
-//         currentUser: { id: 2, username: "test", isAdmin: false },
-//       })
-//     ).toEqual({
-//       2: {
-//         id: 2,
-//         username: "test",
-//         isAdmin: false,
-//       },
-//     });
-//   });
-// });
+  it("handles default state", () => {
+    expect(lineItemsReducer(state, { type: "none" })).toEqual(state);
+  });
+
+  it("handles RECEIVE_LINE_ITEM", () => {
+    const payLoad = {
+      item: {
+        id: 3,
+        product_id: 8,
+        cart_id: 3,
+        quantity: 1,
+      },
+    };
+    expect(
+      lineItemsReducer(state, {
+        type: RECEIVE_LINE_ITEM,
+        item: payLoad.item,
+      })
+    ).toEqual({ ...state, [payLoad.item.id]: payLoad.item });
+  });
+
+  //   it("handles REMOVE_REVIEW", () => {
+  //     expect(
+  //       reviewsReducer(state, {
+  //         type: REMOVE_REVIEW,
+  //         reviewId: state["1"].id,
+  //       })
+  //     ).toEqual({ [state["2"].id]: state["2"] });
+  //   });
+});
 
 // const lineItemsReducer = (state = {}, action) => {
 //   Object.freeze(state);
